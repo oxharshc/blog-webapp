49620233cc317d24ce02fdf03980326100e4c5db
Harshit Chauhan <98049847+oxharshc@users.noreply.github.com>
2026-07-21 19:31:43 +0530

    feat: adding middleware

---
+++ middleware/authentication.js
@@ -0,0 +1,30 @@
+const { validateToken } = require("../services/authentication.js");
+
+function checkForAuthenticationCookie(cookieName) {
+  return (req, res, next) => {
+    const tokenCookieValue = req.cookies[cookieName];
+
+    if (!tokenCookieValue) {
+      return next();
+    }
+
+    try {
+      const userPayload = validateToken(tokenCookieValue);
+      req.user = userPayload;
+    } catch (error) {}
+
+    return next();
+  };
+}
+
+function requireAuth(req, res, next) {
+  if (!req.user) {
+    return res.redirect("/user/signin");
+  }
+  next();
+}
+
+module.exports = {
+  checkForAuthenticationCookie,
+  requireAuth,
+};
